9430eb590c3d6e28bad1e403a789fbd7dd855805
Aaron Winziers <awinziers@gmail.com>
2026-04-14 14:39:57 +0200

    Add ConfigService and .env validation.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { validationSchema } from './config/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema,
+      isGlobal: true,
+      cache: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/config.ts
@@ -0,0 +1,6 @@
+import Joi from 'joi';
+
+export const validationSchema = Joi.object({
+  NODE_ENV: Joi.string().valid('development', 'production', 'test', 'provision').default('development'),
+  PORT: Joi.number().port().default(3000),
+});
