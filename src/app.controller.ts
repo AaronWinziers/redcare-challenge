b615855d05cc03ad9254c0ae7f6f43efeedd9d99
Aaron Winziers <awinziers@gmail.com>
2026-04-14 15:14:49 +0200

    Initialize swagger documentation

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpStatus } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @ApiOperation({ summary: 'Hello World!' })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Hello World!',
+  })
   @Get()
   getHello(): string {
     return this.appService.getHello();
--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { Logger } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 const logger = new Logger('Root');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+
+  const swaggerConfig = new DocumentBuilder().setTitle('Redcare Challenge API').setVersion('0.0.1').build();
+  const documentFactory = () => SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('docs', app, documentFactory);
 
   const port = configService.get<number>('PORT') || 3000;
   logger.log(`Listening on port: ${port}`);
